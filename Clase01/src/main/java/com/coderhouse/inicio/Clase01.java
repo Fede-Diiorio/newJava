46820c87e8ef2f4bd2d1171fec72415ffda7097f
Federico Di Iorio <federico_metallica@hotmail.com>
2024-09-28 16:44:41 -0300

    Declaración de variables de la clase01

---
+++ Clase01/src/main/java/com/coderhouse/inicio/Clase01.java
@@ -0,0 +1,15 @@
+package com.coderhouse.inicio;
+
+public class Clase01 {
+
+	public static void main(String[] args) {
+		// Declaración de variables
+		
+		float numero1 = 1000f;
+		float numero2 = 12f;
+		float resultado = numero1 % numero2;
+		
+		System.out.println("El resultado es: " + resultado);
+
+	}
+}
