91985f766b7092725cc1d3b6a2b0745cc260b247
Federico Di Iorio <federico_metallica@hotmail.com>
2024-09-30 14:28:26 -0300

    Se completó el ejercicio 35

---
+++ Ejercicios/src/main/java/com/coderhouse/integracion/ejercicio35/Main.java
@@ -0,0 +1,17 @@
+package com.coderhouse.integracion.ejercicio35;
+
+public class Main {
+	
+	
+	
+public static void main(String[] args) {
+		
+	VerificarPrimo numero1 = new VerificarPrimo(27);
+	VerificarPrimo numero2 = new VerificarPrimo(17);
+	
+	numero1.esPrimo();
+	numero2.esPrimo();
+
+	}
+	
+}
---
+++ Ejercicios/src/main/java/com/coderhouse/integracion/ejercicio35/VerificarPrimo.java
@@ -0,0 +1,26 @@
+package com.coderhouse.integracion.ejercicio35;
+
+public class VerificarPrimo {
+	
+	int numero;
+	
+	public VerificarPrimo (int numero) {
+		this.numero = numero;
+	}
+	
+	public void esPrimo() {
+		int contador = 0;
+		
+		for (int i = 1; i <= numero; i++) {
+				if(numero % i == 0) {
+					contador++;
+			}
+		}
+		
+		if (contador == 2) {
+			System.out.println("El número " + numero + " es un número primo.");
+		} else {
+			System.out.println("El número " + numero + " NO es un número primo.");
+		}
+	}
+}
