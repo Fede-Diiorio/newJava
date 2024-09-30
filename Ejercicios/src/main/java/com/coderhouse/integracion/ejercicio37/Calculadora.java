aa7f9d9d4a5c72429101083acd1964a9aabd4efb
Federico Di Iorio <federico_metallica@hotmail.com>
2024-09-30 15:38:28 -0300

    Se completó el ejercicio 37

---
+++ Ejercicios/src/main/java/com/coderhouse/integracion/ejercicio37/Calculadora.java
@@ -0,0 +1,33 @@
+package com.coderhouse.integracion.ejercicio37;
+
+public class Calculadora {
+	private int numero1;
+	private int numero2;
+	
+	public Calculadora(int numero1, int numero2) {
+		this.numero1 = numero1;
+		this.numero2 = numero2;
+	}
+	
+	public void sumarNumeros() {
+		System.out.println("El resultado de la suma es: " + (numero1 + numero2));
+	}
+	
+	public void restarNumeros() {
+		System.out.println("El resultado de la resta es: " + (numero1 - numero2));
+	}
+	
+	public void multiplicarNumeros() {
+		System.out.println("El resultado de la multiplicación es: " + (numero1 * numero2));
+	}
+	
+	public void dividirNumeros() {
+		if(numero2 == 0) {
+			System.out.println("No se pudo completar la división dado que el divisor no puede ser igual a 0.");
+		} else {
+			System.out.println("El resultado de la división es: " + (numero1 / numero2));
+		}
+	}
+	
+
+}
---
+++ Ejercicios/src/main/java/com/coderhouse/integracion/ejercicio37/Main.java
@@ -0,0 +1,23 @@
+package com.coderhouse.integracion.ejercicio37;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calculadora figura1 = new Calculadora(5, 5);
+		Calculadora figura2 = new Calculadora(5, -5);
+		
+		figura1.sumarNumeros();
+		figura1.restarNumeros();
+		figura1.multiplicarNumeros();
+		figura1.dividirNumeros();
+		
+		System.out.println("-----");
+		
+		figura2.sumarNumeros();
+		figura2.restarNumeros();
+		figura2.multiplicarNumeros();
+		figura2.dividirNumeros();
+
+	}
+
+}
