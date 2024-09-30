24862a3f890d03a22fb73f9e2d1a95fdd1d143db
Federico Di Iorio <federico_metallica@hotmail.com>
2024-09-30 14:13:03 -0300

    Se completó el ejercicio 34

---
+++ Ejercicios/src/main/java/com/coderhouse/integracion/ejercicio34/CalcularArea.java
@@ -0,0 +1,20 @@
+package com.coderhouse.integracion.ejercicio34;
+
+public class CalcularArea {
+	int numero1;
+	double NUMERO_PI = Math.PI;
+	
+	public CalcularArea (int numero1) {
+		this.numero1 = numero1;
+	}
+	
+	public void calcularAreaDeCirculo() {
+		double resultadoCirculo = NUMERO_PI * numero1 * numero1;
+		System.out.println("El resultado del área de un circulo de radio " + numero1 + " es: " + resultadoCirculo);
+	}
+
+	public void calcularAreaDeCuadrado() {
+		float resultadoCuadrado = numero1 * numero1;
+		System.out.println("El resultado del área de un cuadrado de base " + numero1 + " es: " + resultadoCuadrado);
+	}
+}
---
+++ Ejercicios/src/main/java/com/coderhouse/integracion/ejercicio34/Main.java
@@ -0,0 +1,14 @@
+package com.coderhouse.integracion.ejercicio34;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		CalcularArea figura1 = new CalcularArea(10);
+		
+		figura1.calcularAreaDeCirculo();
+		figura1.calcularAreaDeCuadrado();
+
+	}
+
+}
